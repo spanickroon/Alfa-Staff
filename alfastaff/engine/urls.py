cecf79dee903fed87295d9675dfd06885709ed7c
Nikita Koznev <nikitakoznev@gmail.com>
2020-06-09 19:33:01 +0300

    Added test url

--- alfastaff/engine/urls.py
+++ alfastaff/engine/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def test(request):
+    return HttpResponse('test')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', test)
 ]
