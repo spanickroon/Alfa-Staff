3f2e7a78dd16b6aad8851db943b92517c5c692db
Nikita Koznev <nikitakoznev@gmail.com>
2020-06-09 23:17:30 +0300

    Added allowed hosts localhost

--- engine/settings.py
+++ engine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
