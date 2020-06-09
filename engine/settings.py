4c36eee4c8d03e86c6640b5562d2d7d34fd75bfa
Nikita Koznev <nikitakoznev@gmail.com>
2020-06-09 23:11:53 +0300

    Added database config

--- engine/settings.py
+++ engine/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'engine.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ.get('DATABASE_ENGINE'),
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT')
     }
 }
 
